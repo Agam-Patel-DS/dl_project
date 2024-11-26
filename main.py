f87b6a6b810e96afbb8e2e460e099bd4e7a02a52
Agam-Patel-DS <colabwithagam@gmail.com>
2024-11-26 12:14:25 +0000

    Logger ::> Tested and Succesful

---
+++ main.py
@@ -0,0 +1,4 @@
+#from src.cnnClassifier import logger
+from cnnClassifier import logger #advantange of writing logger in __init__.py
+
+logger.info("Welcome to Custom Logs >> :::: >> Initiating the Code!")
